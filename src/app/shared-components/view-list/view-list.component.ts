33a8198948e6214510bb9a0e61a91731deb3467d
KrishnaKesharwani <31506398+KrishnaKesharwani@users.noreply.github.com>
2025-08-07 14:23:24 +0530

    Listing Design Updated

--- src/app/shared-components/view-list/view-list.component.ts
+++ src/app/shared-components/view-list/view-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input, EventEmitter,Output } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { SingleImageShowComponent } from 'src/app/maintainance-management/single-image-show/single-image-show.component';
 @Component({
@@ -6,38 +6,39 @@ import { SingleImageShowComponent } from 'src/app/maintainance-management/single
   templateUrl: './view-list.component.html',
   styleUrls: ['./view-list.component.scss'],
 })
-export class ViewListComponent  implements OnInit {
-@Input() search:any
-@Input() searchKey:any
-@Input() showSearch:any=true
-@Input() width:any=60
-@Input() list:any=[]
-@Input() keyList:any=[]
-@Input() buttonArray:any=[]
-@Output() actionEventCall=new EventEmitter()
-  constructor(private modalCtrl:ModalController) { }
+export class ViewListComponent implements OnInit {
+  @Input() search: any
+  @Input() searchKey: any
+  @Input() showSearch: any = true
+  @Input() width: any = 60
+  @Input() list: any = []
+  @Input() keyList: any = []
+  @Input() buttonArray: any = []
+  @Input() listColorClass: any;
+  @Output() actionEventCall = new EventEmitter()
+  constructor(private modalCtrl: ModalController) { }
 
   ngOnInit() {
-    console.log("ViewListComponent",this.list,this.search,this.searchKey);
-    
+    console.log("ViewListComponent", this.list, this.search, this.searchKey);
+
   }
-actionEvent(tractor:any,button:any){
-  this.actionEventCall.emit({tractor,button})
-}
-    async viewImage(image:any){
-      const modal = await this.modalCtrl.create({
-        component: SingleImageShowComponent,
-        componentProps: {
-       
-          image: image,
-        },
-      });
-      await modal.present();
-      const { data, role } = await modal.onWillDismiss();
-      console.log('role', role);
-  
-      if (role === 'confirm') {
-     
-      }
+  actionEvent(tractor: any, button: any) {
+    this.actionEventCall.emit({ tractor, button })
+  }
+  async viewImage(image: any) {
+    const modal = await this.modalCtrl.create({
+      component: SingleImageShowComponent,
+      componentProps: {
+
+        image: image,
+      },
+    });
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+    console.log('role', role);
+
+    if (role === 'confirm') {
+
     }
+  }
 }
