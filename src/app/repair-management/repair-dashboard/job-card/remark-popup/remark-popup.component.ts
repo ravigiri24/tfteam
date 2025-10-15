ff4e52ed01303cde1c874b12da22c1253242d5a8
Ravi R Giri <ravisunainagiri@gmail.com>
2025-10-15 13:01:54 +0530

    rem

--- src/app/repair-management/repair-dashboard/job-card/remark-popup/remark-popup.component.ts
+++ src/app/repair-management/repair-dashboard/job-card/remark-popup/remark-popup.component.ts
@@ -22,6 +22,7 @@ export class RemarkPopupComponent  implements OnInit {
     dismiss(){
     this.modalCtrl.dismiss()
   }
+  
   ngOnInit() {
     if(this.jobDetails?.remark){
       this.remark=this.jobDetails?.remark
