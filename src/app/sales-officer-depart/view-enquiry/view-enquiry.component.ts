6822bf01948ce39f032c4b65cfc73f1e8aad4a4a
KrishnaKesharwani <31506398+KrishnaKesharwani@users.noreply.github.com>
2025-11-13 22:42:57 +0530

    View Details design updated

--- src/app/sales-officer-depart/view-enquiry/view-enquiry.component.ts
+++ src/app/sales-officer-depart/view-enquiry/view-enquiry.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import {
   FormGroup,
   FormBuilder,
@@ -14,52 +14,52 @@ import { ViewModelsComponent } from '../add-enquiry/view-models/view-models.comp
   templateUrl: './view-enquiry.component.html',
   styleUrls: ['./view-enquiry.component.scss'],
 })
-export class ViewEnquiryComponent  implements OnInit {
-
+export class ViewEnquiryComponent implements OnInit {
+  @Input() listColorClass = 'sixColor';
   constructor(
     public modalCtrl: ModalController,
     private formBuilder: FormBuilder,
     private share: ShareService,
     private api: ApiService
-  ) {}
+  ) { }
   dismiss() {
     this.modalCtrl.dismiss();
   }
-enquiry:any
-modelList:any=[]
+  enquiry: any
+  modelList: any = []
   ngOnInit() {
-this.modelList=JSON.parse(this.enquiry?.modal_ids)
-        if(this.modelList?.length){
-      let modelList:any=this.modelList
-      modelList?.forEach((model:any)=>{
-   this.getAvailaiblility(model,false)
+    this.modelList = JSON.parse(this.enquiry?.modal_ids)
+    if (this.modelList?.length) {
+      let modelList: any = this.modelList
+      modelList?.forEach((model: any) => {
+        this.getAvailaiblility(model, false)
       })
-   
+
     }
   }
-    getAvailaiblility(model:any,isAdd:any=true){
-      
-    let obj :any= this.share.getListObj('model', true, ['logo'], false);
+  getAvailaiblility(model: any, isAdd: any = true) {
+
+    let obj: any = this.share.getListObj('model', true, ['logo'], false);
     this.share.showLoading('Checking Availaibility');
-    obj.model_id=model?.id
+    obj.model_id = model?.id
     this.api.postapi('getModelAvailaibility', obj).subscribe(
       (res: any) => {
-            let availaibility=res?.data
-     
-      let valueOf = this.modelList;
-      let findModel=valueOf?.findIndex((fn:any)=>fn.id==model.id)
-      valueOf[findModel].availaibility=availaibility
-         // this.form.controls['modal_ids'].setValue(valueOf)
-        
+        let availaibility = res?.data
+
+        let valueOf = this.modelList;
+        let findModel = valueOf?.findIndex((fn: any) => fn.id == model.id)
+        valueOf[findModel].availaibility = availaibility
+        // this.form.controls['modal_ids'].setValue(valueOf)
+
 
         this.share?.spinner?.dismiss();
       },
-      (error: any) => {}
+      (error: any) => { }
     );
   }
- async viewModel(model:any){
- let avaialiableList=model?.availaibility
-   let otherObjects: any;
+  async viewModel(model: any) {
+    let avaialiableList = model?.availaibility
+    let otherObjects: any;
 
     const modal = await this.modalCtrl.create({
       component: ViewModelsComponent,
@@ -73,5 +73,5 @@ this.modelList=JSON.parse(this.enquiry?.modal_ids)
 
     const { data, role } = await modal.onWillDismiss();
   }
-  
+
 }
