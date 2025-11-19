6235e0bf65c9d383a1790d32206b96d22e5aed69
KrishnaKesharwani <31506398+KrishnaKesharwani@users.noreply.github.com>
2025-11-19 11:24:31 +0530

    Design Updated

--- src/app/all-tractor-list/all-tractor-list.component.ts
+++ src/app/all-tractor-list/all-tractor-list.component.ts
@@ -21,32 +21,32 @@ export class AllTractorListComponent implements OnInit {
     public share: ShareService,
     private modalCtrl: ModalController,
     private router: Router,
-    private commonMethod:CommonMethodService
-  ) {}
+    private commonMethod: CommonMethodService
+  ) { }
   alltractorList: any = [];
-    listColorClass = 'sixColor';
-  ngOnInit() {}
+  listColorClass = 'sixColor';
+  ngOnInit() { }
   ionViewWillEnter() {
     this.alltractorList = [];
     this.getBrandList();
     this.getWareHouseList();
-    this.filterBy="ALL"
-    this.listBy="BRAND_WISE"
+    this.filterBy = "ALL"
+    this.listBy = "BRAND_WISE"
     // this.getTractorList();
   }
-    async actionEventCall(e: any) {
-      await  this.commonMethod.actionEventCall(e,{optionsUploadButtonArray:[]})
-    
-  if(this.commonMethod.reloadMethod){
-    this.getTractorList()
-  }
+  async actionEventCall(e: any) {
+    await this.commonMethod.actionEventCall(e, { optionsUploadButtonArray: [] })
+
+    if (this.commonMethod.reloadMethod) {
+      this.getTractorList()
+    }
 
 
     if (this.commonMethod.reloadMethod) {
       this.refreshList();
     }
     console.log('actionEventCall', e);
-    
+
   }
   filterBy: any = 'ALL';
   async presentModal() {
@@ -73,29 +73,29 @@ export class AllTractorListComponent implements OnInit {
       this.callListApi()
     }
   }
-        buttonArray: any = [
- {
+  buttonArray: any = [
+    {
       name: 'Tractor Dashboard',
       action: 'tractorDashboard',
-            closeCurrentPopUP:true,
-    srcPage:'/operational/all-tractor-management',
+      closeCurrentPopUP: true,
+      srcPage: '/operational/all-tractor-management',
       image: './././assets/images/layout.png',
     },
-     {
+    {
       name: 'Sync Mainatainance',
       action: 'syncMainatinance',
       image: './././assets/images/sync.png',
     },
-      {
+    {
       name: 'Tractor Summary',
       action: 'tractorSummary',
       image: './././assets/images/data-analysis.png',
     },
-        
+
   ];
-    keyList: any = [
+  keyList: any = [
     { key: 'Model', value: 'name', type: 'INPUT' },
-        { key: 'TF Code', value: 'registractionNo', type: 'INPUT' },
+    { key: 'TF Code', value: 'registractionNo', type: 'INPUT' },
     { key: 'Engine Number', value: 'engineNumber', type: 'INPUT' },
     // { key: 'Staus', value: 'tractor_status', type: 'INPUT' },
     { key: 'Manufactoring', value: 'yearOfManufactoring', type: 'INPUT' },
@@ -104,8 +104,8 @@ export class AllTractorListComponent implements OnInit {
 
 
     { key: 'Hours', value: 'hours', type: 'INPUT' },
-        { key: 'Transported Place', getFromObj:true,objName:'transportDestination',value: 'name', type: 'INPUT' },
-        { key: 'Franchise(Alloted)', getFromObj:true,objName:'franchiseDettails',value: 'name', type: 'INPUT' },
+    { key: 'Transported Place', getFromObj: true, objName: 'transportDestination', value: 'name', type: 'INPUT' },
+    { key: 'Franchise(Alloted)', getFromObj: true, objName: 'franchiseDettails', value: 'name', type: 'INPUT' },
     { key: 'Registered Date', value: 'createdOn', type: 'DATE' },
   ];
   listBy = 'BRAND_WISE';
@@ -145,7 +145,7 @@ export class AllTractorListComponent implements OnInit {
             //   this.share.spinner?.dismiss();
           }
         },
-        (error: any) => {}
+        (error: any) => { }
       );
     }, 0);
   }
@@ -167,29 +167,29 @@ export class AllTractorListComponent implements OnInit {
     let staffDetails: any = this.share.get_staff();
     this.staffDetails = JSON.parse(staffDetails);
 
-    if(!this.selectedBrand){
-      this.selectedBrand= this.brandList[0]?.id
+    if (!this.selectedBrand) {
+      this.selectedBrand = this.brandList[0]?.id
     }
     let obj = {
       operate: this.staffDetails?.staffCode,
       isLive: true,
       brandId: this.selectedBrand,
-      isDraft:false
+      isDraft: false
     };
-if(loader){
+    if (loader) {
       this.share.showLoading('Loading...');
-   }
-  
+    }
+
     this.api.postapi('getTractorListBranchWiseisLiveNotDraft', obj).subscribe(
       (res: any) => {
         this.alltractorList = res?.data;
         this.allTractorsSrcList = res?.data;
         // this.newArivalsList=this.newArivalsList.filter((f:any)=>f?.tractor_status=='NEW_ARRIVAL')
-         this.sortByFilter()
+        this.sortByFilter()
         this.share.spinner.dismiss('active_six');
         this.backupList = res.data;
       },
-      (error: any) => {}
+      (error: any) => { }
     );
   }
   getAllTractorList(loader: any = false) {
@@ -199,12 +199,12 @@ if(loader){
     let obj = {
       operate: this.staffDetails?.staffCode,
       isLive: true,
- 
+
     };
     //if (loader) {
-      this.share.showLoading('Loading...');
-   // }
- 
+    this.share.showLoading('Loading...');
+    // }
+
     this.api.postapi('getTractorListLiveNotDraft', obj).subscribe(
       (res: any) => {
         this.alltractorList = res?.data;
@@ -214,22 +214,22 @@ if(loader){
         this.share.spinner.dismiss('active_six');
         this.backupList = res.data;
       },
-      (error: any) => {}
+      (error: any) => { }
     );
   }
-     async tractorViewDetail(tractor: any){
-            const modal = await this.modalCtrl.create({
-          component: TractorCostingDashboardComponent,
-          componentProps: {
-         
-            tractor_id: tractor?.id,
-          },
-        });
-        await modal.present();
-        const { data, role } = await modal.onWillDismiss();
-        console.log('role', role);
-     //   this.router.navigate(['/admin-block/view-costing-dashboard', tractor?.id]);
-      }
+  async tractorViewDetail(tractor: any) {
+    const modal = await this.modalCtrl.create({
+      component: TractorCostingDashboardComponent,
+      componentProps: {
+
+        tractor_id: tractor?.id,
+      },
+    });
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+    console.log('role', role);
+    //   this.router.navigate(['/admin-block/view-costing-dashboard', tractor?.id]);
+  }
   selectedStore: any;
   warehouseList: any = [];
   getWareHouseList(loader: any = false) {
@@ -237,7 +237,7 @@ if(loader){
 
     this.staffDetails = JSON.parse(staffDetails);
     //if(loader){
-   // this.share.showLoading('Loading...');
+    // this.share.showLoading('Loading...');
     // }
     let obj: any = this.share.getListObj('warehouselocation', false, [], true);
     obj.storeId = this.staffDetails?.storeId;
@@ -252,16 +252,16 @@ if(loader){
           if (!loader) {
             this.selectedStore = this.warehouseList[0]?.id;
 
-          //  this.getAllTractorListStorewise();
+            //  this.getAllTractorListStorewise();
             //   this.share.spinner?.dismiss();
           }
         },
-        (error: any) => {}
+        (error: any) => { }
       );
     }, 0);
   }
   callListApi() {
-    this.filterBy='ALL'
+    this.filterBy = 'ALL'
     if (this.listBy == 'ALL') {
       this.getAllTractorList();
     } else if (this.listBy == 'BRAND_WISE') {
@@ -272,62 +272,63 @@ if(loader){
 
   }
   sortByFilter() {
-      this.alltractorList=[]
-      setTimeout(() => {
-         if (this.filterBy == 'ALL') {
-         if(this.allTractorsSrcList?.length){
-      this.alltractorList =JSON.parse( JSON.stringify(this.allTractorsSrcList))}
-      else{
-        this.alltractorList =[]
+    this.alltractorList = []
+    setTimeout(() => {
+      if (this.filterBy == 'ALL') {
+        if (this.allTractorsSrcList?.length) {
+          this.alltractorList = JSON.parse(JSON.stringify(this.allTractorsSrcList))
+        }
+        else {
+          this.alltractorList = []
+        }
       }
-    }
-    if (this.filterBy == 'MAPPED') {
-      this.alltractorList = this.allTractorsSrcList.filter(
-        (f: any) => f?.repairMappedData?.length > 0
-      );
-    }
-    if (this.filterBy == 'NOT_MAPPED') {
-      this.alltractorList = this.allTractorsSrcList.filter(
-        (f: any) => f?.repairMappedData?.length == 0
-      );
-    }
-    if (this.filterBy == 'SOLD') {
-      this.alltractorList = this.allTractorsSrcList.filter(
-        (f: any) => f?.isSold == 1
-      );
-    }
-    if (this.filterBy == 'NOT_SOLD') {
-      this.alltractorList = this.allTractorsSrcList.filter(
-        (f: any) => f?.isSold == 0
-      );
-    }
-      }, 0);
-   
+      if (this.filterBy == 'MAPPED') {
+        this.alltractorList = this.allTractorsSrcList.filter(
+          (f: any) => f?.repairMappedData?.length > 0
+        );
+      }
+      if (this.filterBy == 'NOT_MAPPED') {
+        this.alltractorList = this.allTractorsSrcList.filter(
+          (f: any) => f?.repairMappedData?.length == 0
+        );
+      }
+      if (this.filterBy == 'SOLD') {
+        this.alltractorList = this.allTractorsSrcList.filter(
+          (f: any) => f?.isSold == 1
+        );
+      }
+      if (this.filterBy == 'NOT_SOLD') {
+        this.alltractorList = this.allTractorsSrcList.filter(
+          (f: any) => f?.isSold == 0
+        );
+      }
+    }, 0);
+
   }
   getAllTractorListStorewise(loader: any = false) {
     let staffDetails: any = this.share.get_staff();
     this.staffDetails = JSON.parse(staffDetails);
-    if(!this.selectedStore){
-      this.selectedStore= this.warehouseList[0]?.id
+    if (!this.selectedStore) {
+      this.selectedStore = this.warehouseList[0]?.id
     }
     let obj = {
       operate: this.staffDetails?.staffCode,
       store_id: this.selectedStore,
     };
     //if (loader) {
-      this.share.showLoading('Loading...');
+    this.share.showLoading('Loading...');
     //}
     this.api.postapi('getTractorsListStoreWiseNotDraft', obj).subscribe(
       (res: any) => {
         this.alltractorList = res?.data;
         this.allTractorsSrcList = res?.data;
         // this.newArivalsList=this.newArivalsList.filter((f:any)=>f?.tractor_status=='NEW_ARRIVAL')
-        
-      this.sortByFilter()
+
+        this.sortByFilter()
         this.share.spinner.dismiss('active_six');
         this.backupList = res.data;
       },
-      (error: any) => {}
+      (error: any) => { }
     );
   }
   async viewImage(tractor: any) {
@@ -364,16 +365,16 @@ if(loader){
       '/operational/all-tractor-management',
     ]);
   }
-      async searchTractor() {
+  async searchTractor() {
     const modal = await this.modalCtrl.create({
       component: SearchTractorWithTfCodeComponent,
       componentProps: {
-       buttonArray: this.buttonArray,
-       listColorClass:this.listColorClass,
-       keyList:this.keyList,
-       searchFilter:this.search,
-       searchKey:'registractionNo',
-     obj:{optionsUploadButtonArray:[]}
+        buttonArray: this.buttonArray,
+        listColorClass: this.listColorClass,
+        keyList: this.keyList,
+        searchFilter: this.search,
+        searchKey: 'registractionNo',
+        obj: { optionsUploadButtonArray: [] }
       },
     });
     await modal.present();
--- src/app/shared-components/view-list/view-list.component.ts
+++ src/app/shared-components/view-list/view-list.component.ts
@@ -8,7 +8,7 @@ import { ImageSliderComponent } from '../image-slider/image-slider.component';
   selector: 'app-view-list',
   templateUrl: './view-list.component.html',
   styleUrls: ['./view-list.component.scss'],
-    animations: [
+  animations: [
     trigger('showMoreDetails', [
       state('false', style({ maxHeight: 0 })),
       state('true', style({ maxHeight: '500px' })),
@@ -24,7 +24,7 @@ export class ViewListComponent implements OnInit {
   @Input() width: any = 60;
   @Input() list: any = [];
   @Input() keyList: any = [];
-  @Input() rowLineCount= 3;
+  @Input() rowLineCount = 3;
   @Input() buttonArray: any = [];
   @Input() listColorClass: any;
   @Output() actionEventCall = new EventEmitter();
@@ -46,7 +46,7 @@ export class ViewListComponent implements OnInit {
     this.activeCardIndex = null;
     this.activeCardIndex = index;
     setTimeout(() => {
-      this.showOverlay = true; 
+      this.showOverlay = true;
     }, 100);
     // this.photoCount++;
   }
@@ -65,15 +65,17 @@ export class ViewListComponent implements OnInit {
     );
     tractor.imagesToShow = imagesToShow;
   }
-  
 
-showMore(index: number, keyValue: any) {
-  this.showMoreDetails[index] = !this.showMoreDetails[index];
-  // You can add animation logic here if needed
-}
-actionEvent(tractor: any, button: any) {
-  this.actionEventCall.emit({ tractor, button });
-}
+
+  showMore(index: number, keyValue: any) {
+    this.showMoreDetails[index] = !this.showMoreDetails[index];
+    // You can add animation logic here if needed
+  }
+  actionEvent(tractor: any, button: any) {
+    setTimeout(() => {
+      this.actionEventCall.emit({ tractor, button });
+    }, 600);
+  }
   async viewImage(image: any) {
     const modal = await this.modalCtrl.create({
       component: SingleImageShowComponent,
@@ -88,23 +90,23 @@ actionEvent(tractor: any, button: any) {
     if (role === 'confirm') {
     }
   }
-      async viewInSlider(image: any,imageArray:any) {
-      const modal = await this.modalCtrl.create({
-        component: ImageSliderComponent,
-            cssClass: 'midium-model',
-        componentProps: {
-   
-          image: image,
-          imageArray:imageArray
-        },
-      });
-      await modal.present();
-      const { data, role } = await modal.onWillDismiss();
-      console.log('role', role);
-  
-      if (role === 'confirm') {
-   
-      }
+  async viewInSlider(image: any, imageArray: any) {
+    const modal = await this.modalCtrl.create({
+      component: ImageSliderComponent,
+      cssClass: 'midium-model',
+      componentProps: {
+
+        image: image,
+        imageArray: imageArray
+      },
+    });
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+    console.log('role', role);
+
+    if (role === 'confirm') {
+
     }
-    searchText:any=''
+  }
+  searchText: any = ''
 }
