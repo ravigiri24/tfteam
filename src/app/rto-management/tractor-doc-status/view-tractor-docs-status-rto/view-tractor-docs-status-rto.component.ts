16c0ec18c36d095710df83452763cf3ae2c84e76
KrishnaKesharwani <31506398+KrishnaKesharwani@users.noreply.github.com>
2025-12-29 13:37:07 +0530

    Design Updated

--- src/app/rto-management/tractor-doc-status/view-tractor-docs-status-rto/view-tractor-docs-status-rto.component.ts
+++ src/app/rto-management/tractor-doc-status/view-tractor-docs-status-rto/view-tractor-docs-status-rto.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { AddTractorDocsStatusRtoComponent } from '../add-tractor-docs-status-rto/add-tractor-docs-status-rto.component';
 import { ShareService } from 'src/app/share.service';
@@ -8,82 +8,82 @@ import { ApiService } from 'src/app/api.service';
   templateUrl: './view-tractor-docs-status-rto.component.html',
   styleUrls: ['./view-tractor-docs-status-rto.component.scss'],
 })
-export class ViewTractorDocsStatusRtoComponent  implements OnInit {
+export class ViewTractorDocsStatusRtoComponent implements OnInit {
 
-  constructor(private modalCtrl:ModalController,private share:ShareService,private api:ApiService) { }
-  dismiss(){
-this.modalCtrl.dismiss()
+  constructor(private modalCtrl: ModalController, private share: ShareService, private api: ApiService) { }
+  dismiss() {
+    this.modalCtrl.dismiss()
   }
-listColorClass='fourthColor'
-tractorDetails:any
+  @Input()listColorClass = 'secondColor';
+  tractorDetails: any
   ngOnInit() {
     this.getNotList()
   }
-note_list:any=[
-  {note:"Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai"},
-  {note:"Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai"}
+  note_list: any = [
+    { note: "Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai" },
+    { note: "Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai Application mai naam galat likha hua hai" }
 
-]
-async enterStatus(note_data:any=null,ind:any=null,note:any=null){
-   const modal = await this.modalCtrl.create({
-      component: AddTractorDocsStatusRtoComponent,
-      componentProps: {
-        tractorDetails: this.tractorDetails,
-        type:'STATUS',
-        editData:note?.status||null,
-        title:'Status',
-        note_id:note_data?.note?.id
-      },
-    });
-    await modal.present();
-    const { data, role } = await modal.onWillDismiss();
-    console.log('role', role);
-    if(data?.row){
-      this.note_list[ind].status=data?.row
-    }
-}
-  async addNewNotes(note:any=null,ind:any=null) {
+  ]
+  async enterStatus(note_data: any = null, ind: any = null, note: any = null) {
     const modal = await this.modalCtrl.create({
       component: AddTractorDocsStatusRtoComponent,
       componentProps: {
         tractorDetails: this.tractorDetails,
-        type:'NOTE',
-        editData:note,
-        title:'Note'
+        type: 'STATUS',
+        editData: note?.status || null,
+        title: 'Status',
+        note_id: note_data?.note?.id
       },
     });
     await modal.present();
     const { data, role } = await modal.onWillDismiss();
     console.log('role', role);
-    if(data?.row){
-      if(!note){
-   this.note_list.unshift({note:data?.row,status:null})
-      }else{
-   this.note_list[ind].note=data?.row
-      }
-   
+    if (data?.row) {
+      this.note_list[ind].status = data?.row
     }
   }
-    stateList: any = [];
-    allData:any=[]
-    notes_raw:any
+  async addNewNotes(note: any = null, ind: any = null) {
+    const modal = await this.modalCtrl.create({
+      component: AddTractorDocsStatusRtoComponent,
+      componentProps: {
+        tractorDetails: this.tractorDetails,
+        type: 'NOTE',
+        editData: note,
+        title: 'Note'
+      },
+    });
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+    console.log('role', role);
+    if (data?.row) {
+      if (!note) {
+        this.note_list.unshift({ note: data?.row, status: null })
+      } else {
+        this.note_list[ind].note = data?.row
+      }
+
+    }
+  }
+  stateList: any = [];
+  allData: any = []
+  notes_raw: any
   getNotList() {
-    
+
     this.share.showLoading('Loading');
-    let obj :any= this.share.getListObj('list', false, [], true);
-    obj.tractor_id=this.tractorDetails?.id
+    let obj: any = this.share.getListObj('list', false, [], true);
+    obj.tractor_id = this.tractorDetails?.id
     this.api.postapi('rto_note_list', obj).subscribe(
       (res: any) => {
-        this.note_list=[]
-        this.notes_raw = res?.data?.filter((f:any)=>f.type=='NOTE');
+        this.note_list = []
+        this.notes_raw = res?.data?.filter((f: any) => f.type == 'NOTE');
         this.allData = res?.data
-            this.notes_raw?.forEach((note:any)=>{
-              let findStatus= this.allData?.find((status:any)=>status?.type=='STATUS' && status?.note_id==note?.id) 
-              this.note_list.push({note:note,status:findStatus||null})
-            }) 
-     this.share.spinner.dismiss('active_four')
+        this.notes_raw?.forEach((note: any) => {
+          let findStatus = this.allData?.find((status: any) => status?.type == 'STATUS' && status?.note_id == note?.id)
+          this.note_list.push({ note: note, status: findStatus || null })
+        })
+        this.share.spinner.dismiss('active_four')
       },
-      (error: any) => {}
+      (error: any) => { }
     );
   }
 }
